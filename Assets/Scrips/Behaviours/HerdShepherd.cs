be1d9375306ece6886e0c09a9d8095999ddf512c
IlyaLaska <IlyaLaska@users.noreply.github.com>
2020-12-10 00:11:58 +0200

    a

--- Assets/Scrips/Behaviours/HerdShepherd.cs
+++ Assets/Scrips/Behaviours/HerdShepherd.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HerdShepherd : MonoBehaviour
+public class HerdShepherd: AbstractBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Doe doe;
+    CircleCollider2D objectBody;
+    List<GameObject> groupList;
+    private void Start()
     {
-        
+        base.Start();
+        doe = GetComponent<Doe>();
+        objectBody = GetComponent<CircleCollider2D>();
+        groupList = GM.instance.doeList[doe.groupId];
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public override Vector3 GetDesiredVelocity()
     {
         
+        return new Vector3(0, 0, 0);
+    }
+
+    private Vector3 Separate() 
+    {
+        float desiredseparation = objectBody.radius*2
+
     }
 }
+
+
