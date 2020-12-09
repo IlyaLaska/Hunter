bbac732cf43f6708ddfa91c5fa4c3a6b40b49a80
IlyaLaska <IlyaLaska@users.noreply.github.com>
2020-12-09 21:52:22 +0200

    tweak

--- Assets/Scrips/Behaviours/AvoidCliffs.cs
+++ Assets/Scrips/Behaviours/AvoidCliffs.cs
@@ -9,25 +9,26 @@ public class AvoidCliffs : AbstractBehaviour
     public override Vector3 GetDesiredVelocity()
     {
         var maxSpeed = movable.VelocityLimit;
-        
+
+        float rnd = Random.Range(-maxSpeed, maxSpeed);
         if (transform.position.x >= edgeX)
         {
             //Debug.Log("RUN: " + new Vector3(-maxSpeed, 0, 0));
-            return new Vector3(-maxSpeed, 0, 0);
+            return new Vector3(-maxSpeed, rnd, 0).normalized * maxSpeed;
 
         }
         if (transform.position.x <= -edgeX)
         {
             //Debug.Log("RUN");
-            return new Vector3(maxSpeed, 0, 0);
+            return new Vector3(maxSpeed, rnd, 0).normalized * maxSpeed;
         }
         if (transform.position.y >= edgeY)
         {
-            return new Vector3(0, -maxSpeed, 0);
+            return new Vector3(rnd, -maxSpeed, 0).normalized * maxSpeed;
         }
         if (transform.position.y <= -edgeY)
         {
-            return new Vector3(0, maxSpeed, 0);
+            return new Vector3(rnd, maxSpeed, 0).normalized * maxSpeed;
         }
 
         return Vector3.zero;
